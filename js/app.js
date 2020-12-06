70f2c0689d447e30f721e33cd78b41cd100d8442
kazuya hojo <maboai.kazu29@gmail.com>
2020-12-06 10:08:55 +0900

    リファクタリング

--- js/app.js
+++ js/app.js
@@ -15,16 +15,19 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 
-
-function draw() {
-  // Canvasの内容を消去するメソッド、clearRect()がある
-  ctx.clearRect(0,0,canvas.width, canvas.height);
+function drawBall() {
   ctx.beginPath();
   ctx.arc(x,y,10, 0, Math.PI * 2);
   ctx.fillStyle = "#0095DD";
   ctx.fill();
   ctx.closePath();
-  
+}
+
+
+function draw() {
+  // Canvasの内容を消去するメソッド、clearRect()がある
+  ctx.clearRect(0,0,canvas.width, canvas.height);
+  drawBall()
 
   // xとyに毎フレーム描画した後に小さな値を加え、ボールが動いているように見せる
   // しかし毎回描画しているので軌跡が残る
